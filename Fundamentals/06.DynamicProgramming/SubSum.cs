3d9d4e708d43650e43411646eb17ff258dbd5e11
Pier Stoyanov <pier.stoyanov@gmail.com>
2022-07-29 22:55:43 +0300

    sub sum all combinations

---
+++ Fundamentals/06.DynamicProgramming/SubSum.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+class SubSum
+{
+    static void Main(string[] args)
+    {
+        var nums = new[] { 3, 5, 1, 4, 2 };
+        var possibleSums = GetAllPossibleSums(nums);
+    
+    }
+
+    private static HashSet<int> GetAllPossibleSums(int[] nums)
+    {
+        var sums = new HashSet<int> { 0 };
+
+        foreach (var num in nums)
+        {
+            var newSums = new HashSet<int>();
+
+            foreach (var sum in sums)
+            {
+                newSums.Add(sum + num);
+            }
+
+            sums.UnionWith(newSums);
+        }
+
+        return sums;
+    }
+}
