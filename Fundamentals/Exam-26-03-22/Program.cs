6f672881d486e969cf363adb2f76346bc8464d7e
Pier Stoyanov <pier.stoyanov@gmail.com>
2022-07-29 12:54:59 +0300

    added exam 26-03-22

---
+++ Fundamentals/Exam-26-03-22/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exam_26_03_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
