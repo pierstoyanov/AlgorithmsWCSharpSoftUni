f069684d54b43de1494a12cefdcce0d6da60773f
Pier Stoyanov <pier.stoyanov@gmail.com>
2022-07-29 22:46:02 +0300

    fibbonacci with memoization

---
+++ Fundamentals/06.DynamicProgramming/01.FIbDynamicP.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class FibWithMemo
+{   
+    private static Dictionary<int, long> cache;
+    static void Main(string[] args)
+    {
+        var n = int.Parse(Console.ReadLine());
+        cache = new Dictionary<int, long>();
+
+        Console.WriteLine(GenerateFib(n));
+
+    }
+
+    private static long GenerateFib(int n)
+    {
+        if (n < 2)
+        {
+            return n;
+        }
+
+        if (cache.ContainsKey(n))
+        {
+            return cache[n];
+        }
+
+        var result = GenerateFib(n - 1) + GenerateFib(n - 2);
+        cache[n] = result;
+
+        return result;
+    }
+}
