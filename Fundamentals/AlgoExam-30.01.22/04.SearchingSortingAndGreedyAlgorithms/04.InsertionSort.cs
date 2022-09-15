0fbd1f34f3743539a2f1b32067e3bdc563f307f1
Pier Stoyanov <pier.stoyanov@gmail.com>
2022-09-15 16:16:51 +0300

    insertion sort

---
+++ Fundamentals/AlgoExam-30.01.22/04.SearchingSortingAndGreedyAlgorithms/04.InsertionSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.SearchingSortingAndGreedyAlgorithms
+{
+    class InsertionSortProgram
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var result = InsertionSort(arr);
+            Console.WriteLine(string.Join(' ', result));
+        }
+
+        private static int[] InsertionSort(int[] arr)
+        {
+            //loop trough entire array
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var current = arr[i];
+                var j = i - 1;
+
+                while (j >= 0 && arr[j] > current)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = current;
+            }
+
+            return arr;
+        }
+    }
+}
