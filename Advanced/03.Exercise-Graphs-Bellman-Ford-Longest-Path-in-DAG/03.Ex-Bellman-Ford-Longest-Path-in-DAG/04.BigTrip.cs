bde3c1b832955973c76931e2371ff7ed5c84398c
Pier Stoyanov <pier.stoyanov@gmail.com>
2022-11-09 17:21:44 +0200

    04.Big Trip / Longest path in DAG

---
+++ Advanced/03.Exercise-Graphs-Bellman-Ford-Longest-Path-in-DAG/03.Ex-Bellman-Ford-Longest-Path-in-DAG/04.BigTrip.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _03.Ex_Bellman_Ford_Longest_Path_in_DAG
+{
+    public static class BigTrip
+    {
+        private class Edge 
+        {
+            public int Source { get; set; }
+            public int Destination { get; set; }
+            public int Time { get; set; }
+        }
+
+        private static Dictionary<int, List<Edge>> nodeEdges;
+        private static double[] distance;
+        private static int[] previous;
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            int e = int.Parse(Console.ReadLine());
+
+            nodeEdges = new Dictionary<int, List<Edge>>();
+            for (int i = 0; i < e; i++)
+            {
+                var edgeData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                var source = edgeData[0];
+                var destination = edgeData[1];
+                var time = edgeData[2];
+
+                if (!nodeEdges.ContainsKey(source))
+                {
+                    nodeEdges.Add(source, new List<Edge>());
+                }
+
+                if (!nodeEdges.ContainsKey(destination))
+                {
+                    nodeEdges.Add(destination, new List<Edge>());
+                }
+
+                nodeEdges[source].Add(new Edge 
+                {
+                    Source = source,
+                    Destination = destination,
+                    Time = time
+                });
+
+            }
+
+            var targetSource = int.Parse(Console.ReadLine());
+            int targetDestination = int.Parse(Console.ReadLine());
+
+            distance = new double[n + 1];
+            for (int i = 0; i < n + 1; i++)
+            {
+                distance[i] = double.NegativeInfinity;
+            }
+            distance[targetSource] = 0;
+
+            previous = new int[n + 1];
+            for (int i = 0; i < n + 1; i++)
+            {
+                previous[i] = -1;
+            }
+
+
+            var sortedNodes = TopologicalDFS();
+
+            while (sortedNodes.Count > 0)
+            {
+                var node = sortedNodes.Pop();
+
+                foreach (var edge in nodeEdges[node])
+                 {
+                    var newTime = distance[edge.Source] + edge.Time;
+
+                    if (newTime > distance[edge.Destination])
+                    {
+                        distance[edge.Destination] = newTime;
+                        previous[edge.Destination] = edge.Source;
+                    }
+                }
+            }
+
+
+            Console.WriteLine(distance[targetDestination]);
+            PrintPath(previous, targetDestination);
+        }
+
+        private static void PrintPath(int[] previous, int targetDestination)
+        {
+            var path = new Stack<int>();
+            var node = targetDestination;
+            while (node != -1)
+            {
+                path.Push(node);
+                node = previous[node];
+            }
+
+            Console.WriteLine(string.Join(" ", path));
+        }
+
+        private static Stack<int> TopologicalDFS()
+        {
+            var result = new Stack<int>();
+            var visited = new HashSet<int>();
+
+            foreach (var node in nodeEdges.Keys)
+            {
+                DFS(node, visited, result);
+            }
+
+            return result;
+        }
+
+        private static void DFS(int node, HashSet<int> visited, Stack<int> result)
+        {
+            if (visited.Contains(node))
+            {
+                return;
+            }
+
+            visited.Add(node);
+
+            foreach (var edge in nodeEdges[node])
+            {
+                DFS(edge.Destination, visited, result);
+            }
+
+            result.Push(node);
+        }
+    }
+}
---
+++ Advanced/03.Exercise-Graphs-Bellman-Ford-Longest-Path-in-DAG/03.Ex-Bellman-Ford-Longest-Path-in-DAG/3.Undefined.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Ex_Bellman_Ford_Longest_Path_in_DAG
+{
+    public class Undefined
+    {
+        public class Edge
+        {
+            public int To { get; set; }
+            public int From { get; set; }
+            public int Weight { get; set; }
+        }
+
+        // Структури от данни
+        private static List<Edge> graph = new List<Edge>();
+        private static double[] distance;
+        private static int[] previous;
+
+        public static void Main(string[] args)
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var edges = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < edges; i++)
+            {
+                var edgeData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                graph.Add(new Edge
+                {
+                    From = edgeData[0],
+                    To = edgeData[1],
+                    Weight = edgeData[2]
+                });
+            }
+
+            var source = int.Parse(Console.ReadLine());
+            var destination = int.Parse(Console.ReadLine());
+
+            // масив за дистанциите
+            distance = new double[nodes + 1];
+            /*Array.Fill(distance, double.PositiveInfinity)*/;
+            for (int i = 0; i < nodes + 1; i++)
+            {
+                distance[i] = double.PositiveInfinity;
+            }
+            distance[source] = 0;
+
+            // масив за пътя
+            previous = new int[nodes + 1];
+            /*Array.Fill(previous, -1);*/
+            for (int i = 0; i < nodes + 1; i++)
+            {
+                previous[i] = -1;
+            }
+
+            for (int i = 0; i < nodes - 1; i++)
+            {
+                var updated = false;
+
+                foreach (var edge in graph)
+                {
+                    if (double.IsPositiveInfinity(distance[edge.From]))
+                    {
+                        continue;
+                    }
+
+                    var newDistance = distance[edge.From] + edge.Weight;
+
+                    if (newDistance < distance[edge.To])
+                    {
+                        distance[edge.To] = newDistance;
+                        previous[edge.To] = edge.From;
+                        updated = true;
+                    }
+
+                    if (!updated)
+                    {
+                        break;
+                    }
+                }
+            }
+            foreach (var edge in graph)
+            {
+                // при повтарянето на foreach,
+                // ако има по-малка дистанция от намерените,
+                // значи попада в негативен цикъл
+                var newDistance = distance[edge.From] + edge.Weight;
+
+                if (newDistance < distance[edge.To])
+                {
+                    Console.WriteLine("Undefined");
+                    return;
+                }
+            }
+
+            PrintPath(previous, destination);
+            Console.WriteLine(distance[destination]);
+        }
+
+        private static void PrintPath(int[] previous, int destination)
+        {
+            var path = new Stack<int>();
+            var node = destination;
+            while (node != -1)
+            {
+                path.Push(node);
+                node = previous[node];
+            }
+
+            Console.WriteLine(string.Join(" ", path));
+        }
+    }
+}
