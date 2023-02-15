0dd49b6f554da2f2d4f01bfc052ce9daf17ce3f0
Pier Stoyanov <pier.stoyanov@gmail.com>
2023-02-15 15:14:53 +0200

    articulation points

--- Advanced/04.Graphs-Strongly-Connected-Components-Max-Flow/03.ArticulationPoints.cs
+++ Advanced/04.Graphs-Strongly-Connected-Components-Max-Flow/03.ArticulationPoints.cs
@@ -5,11 +5,105 @@ using System.Linq;
 
 namespace ArticulationPoints
 {
-    public class EdmondsKarp
+    public class HopcroftTarjan
     {
+        private static List<int>[] graph;
+        private static bool[] visited;
+        private static int[] depth;
+        private static int[] lowpoint;
+        private static int[] parent;
+
+        private static List<int> articulationPoints;
+
         static void Main(string[] args)
         {
+            var nodes = int.Parse(Console.ReadLine());
+            var lines = int.Parse(Console.ReadLine());
 
+            graph = new List<int>[nodes];
+            visited = new bool[nodes];
+            depth = new int[nodes];
+            lowpoint = new int[nodes];
+            parent = new int[nodes];
+            Array.Fill(parent, -1);
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                graph[node] = new List<int>();
+            }
+
+            for (int l = 0; l < lines; l++)
+            {
+                var line = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                
+                var node = line[0];
+
+                // undirected graph
+                for (int j = 0; j < line.Length; j++)
+                {
+                    var child = line[j];
+                    graph[node].Add(child);
+                    graph[child].Add(node);
+                }
+
+
+               // for directed graph
+               // graph[node].AddRange(line.Skip(1));
+            }
+
+
+            articulationPoints = new List<int>();
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                if (visited[node])
+                {
+                    continue;
+                }
+
+                FindArticulationPoint(node, 1);
+            }
+            
+            Console.WriteLine($"Articulation points: {string.Join(", ", articulationPoints)}");
+        }
+
+        private static void FindArticulationPoint(int node, int currentDepth)
+        {
+            visited[node] = true;
+            depth[node] = currentDepth;
+            lowpoint[node] = currentDepth;
+
+            var childCount = 0;
+            var isArticulationPoint = false;
+
+            foreach (var child in graph[node])
+            {
+                if (!visited[child])
+                {
+                    parent[child] = node;
+                    FindArticulationPoint(child, currentDepth += 1);
+                    
+                    childCount += 1;
+
+                    if (lowpoint[child] >= depth[node])
+                    {
+                        isArticulationPoint = true;
+                    }
+
+                    lowpoint[node] = Math.Min(lowpoint[node], lowpoint[child]);
+                }
+                else if (parent[node] != child)
+                {
+                    lowpoint[node] = Math.Min(lowpoint[node], depth[child]);
+                }
+            }
+
+            // check for root
+            if ((parent[node] == -1 && childCount > 1) ||
+                (parent[node] != -1 && isArticulationPoint))
+            {
+                articulationPoints.Add(node);
+            }
         }
     }
-}
+} 
